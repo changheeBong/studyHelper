2261e883d1caaf429c83d95ddf1fce721c81c4e0
eodovo <eodovo@naver.com>
2023-01-21 20:00:39 +0900

    로그인 마크업

---
+++ src/main/front/src/App.tsx
@@ -0,0 +1,12 @@
+import Navigator from "./pages/Navigator";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigator />
+    </div>
+  );
+}
+
+export default App;
